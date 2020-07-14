486be7fc063ce768d8df75ed4527d0107baa7359
unknown <fattybeatz403@gmail.com>
2020-07-14 13:40:34 -0600

    + added .firebase to .gitignore + added basic unit test for render without crash

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
 it("renders without crashing", () => {
-  window.scrollTo = jest.fn()
+  window.scrollTo = jest.fn();
+
+  const div = document.createElement('div');
+  
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 })
 
 test('renders homepage header', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/homepage_heading/i);
+
   expect(linkElement).toBeInTheDocument();
 });
